26bae7bcf5272cb81764c34b4119f052e4edd57d
Vignesh <vickyprakasy20@gmail.com>
2023-09-07 11:41:03 +0530

    add java-project main

---
+++ Age.java
@@ -0,0 +1,11 @@
+class Age{
+public static void main(String[] args){
+int age=17;
+if (age >= 18){
+System.out.println("You are eligible to vote!");
+}
+else{
+System.out.println("You are not eligible to vote.");
+}
+}
+}
