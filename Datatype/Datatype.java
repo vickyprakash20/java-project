a970f786de18bb51fd092c8b5f09c78fe14e83ca
Vignesh <vickyprakasy20@gmail.com>
2023-09-14 17:42:31 +0530

    commit message

---
+++ Datatype/Datatype.java
@@ -0,0 +1,29 @@
+class Datatype{
+public static void main(String []arg){
+String a;
+a="Today java datatype class";
+System.out.println(a);
+boolean b;
+b=true;
+b=false;
+System.out.println(b);
+char c;
+c=65;
+System.out.println(c);
+byte x;
+x=20;
+System.out.println(x);
+int y;
+y=300;
+System.out.println(y);
+short z;
+z=1000;
+System.out.println(z);
+float d;
+d=5.21f;
+System.out.println(d);
+double e;
+e=21.200001;
+System.out.println(e);
+}
+}
